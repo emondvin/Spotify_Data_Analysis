29241db2b35014a9cff10488fd122c0702a046c9
Vincent Emond <emondvin@Vincents-MacBook-Pro.local>
2020-04-25 13:15:47 -0400

    Fix directories for data analysis

--- EDA_0.3_pd.py
+++ EDA_0.3_pd.py
@@ -8,9 +8,9 @@ from wordcloud import *
 
 
 #Pull the files
-sp_files = glob.glob("sp*[2018,2019,n].csv")
+sp_files = glob.glob("SpApp/sp*[2018,2019,n].csv")
 sp_files
-my_files = glob.glob("[!sp]*[2018,19].csv")
+my_files = glob.glob("User/*[2018,19].csv")
 my_files
 
 
@@ -35,13 +35,13 @@ spmean = mean_features(sp_df)
 
 plot_df = pd.DataFrame({'My Music': mymean, 'Spotify': spmean}, index = spmean.index)
 ax = plot_df.plot.bar(title = 'Average audio features for top of year playlists: 2018 & 2019')
-avg_plt_fig = ax.get_figure().savefig('Average.png')
+avg_plt_fig = ax.get_figure().savefig('Average.png', bbox_inches = 'tight')
 
 
-sp2018df = pd.read_csv('sp_top_2018.csv')
-sp2019df = pd.read_csv('sp_top_2019.csv')
-my2018df = pd.read_csv('top_2018.csv')
-my2019df = pd.read_csv('top_2019.csv')
+sp2018df = pd.read_csv('SpApp/sp_top_2018.csv')
+sp2019df = pd.read_csv('SpApp/sp_top_2019.csv')
+my2018df = pd.read_csv('User/top_2018.csv')
+my2019df = pd.read_csv('User/top_2019.csv')
 
 mymean18 = mean_features(my2018df)
 mymean19 = mean_features(my2019df)
@@ -53,7 +53,7 @@ plotdf = pd.DataFrame({'My Music 2018' : mymean18, 'My Music 2019': mymean19,
 
 plotdf
 axe = plotdf.plot.bar(title = 'Average audio features for top of year playlists: 2018 & 2019')
-dis_plot_fig = axe.get_figure().savefig('Discrete_plot.png')
+dis_plot_fig = axe.get_figure().savefig('Discrete_plot.png', bbox_inches = 'tight')
 
 
 # -----------------------------------------------------
